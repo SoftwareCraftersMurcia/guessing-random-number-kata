e47acb3d994fa13180047e3579626d0080c0527c
Manuel Romero <manuelromero@paradigmadigital.com>
2022-10-06 19:25:55 +0200

    second red test

--- python/tests/test_main.py
+++ python/tests/test_main.py
@@ -9,3 +9,16 @@ def test_given_a_number_always_is_true():
 
     # then
     assert result == "WIN"
+
+def test_given_a_five_when_target_is_three_results_lower():
+    # given
+    number = 5
+
+    # when
+    result = MainClass.main_function(number)
+
+    # then
+    assert result == "LOWER"
+
+
+
