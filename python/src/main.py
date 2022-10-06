ee86b707251d435171f921bc24850d2a168518cb
Manuel Romero <manuelromero@paradigmadigital.com>
2022-10-06 19:49:08 +0200

    green third test

--- python/src/main.py
+++ python/src/main.py
@@ -1,7 +1,10 @@
 class MainClass:
 
     def main_function(number):
+
         if number == 5:
             return "LOWER"
+        if number == 1:
+            return "HIGHER"
 
         return "WIN"
